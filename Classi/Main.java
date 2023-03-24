e502d090d1249c38d27b0e444cab5a45628fa3c3
Gabriele <gabrieleingrassia3@gmail.com>
2023-03-24 16:47:52 +0100

    creata classe PrimiPiatti e Main

---
+++ Classi/Main.java
@@ -0,0 +1,12 @@
+package Classi;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        PrimoPiatto carbonara= new PrimoPiatto("Carbonara", 11.1234557);
+        PrimoPiatto.print(carbonara);
+        carbonara.printer();
+
+    }
+}

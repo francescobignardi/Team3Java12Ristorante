a494e15ce9c9b554fd03bc1d1e2b514295e6f4f5
michelepc <mangiacotti.fi@gmail.com>
2023-04-07 17:29:09 +0200

    Code review 07/04

--- src/Menu.java
+++ src/Menu.java
@@ -1,14 +1,22 @@
 package src;
 
+import java.util.ArrayList;
+
 public class Menu {
     private String menu;
     private String chef;
     private String tipoMenu;
 
+    private ArrayList<src.Antipasti> antipastiList;
+
+    //TODO ognuno di voi dovrà aggiungere una lista con metodi add e remove e un metodo printMenu per stampare
+    // tutte le portate quindi un for su ogni lista
+
     public Menu(String menu, String chef, String tipoMenu) {
         this.menu = menu;
         this.chef = chef;
         this.tipoMenu = tipoMenu;
+        this.antipastiList = new ArrayList<>();
     }
 
     public String getMenu() {
